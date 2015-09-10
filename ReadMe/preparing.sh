5fa44229761e1fd5e236c07c7bbea1299d9ad895
bijan biaee <bijan@binaee.com>
2015-09-10 12:20:20 +0430

    Fix sudo problem due to ubuntu users

--- ReadMe/preparing.sh
+++ ReadMe/preparing.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-chgrp uucp /var/lock/lockdev
+USERNAME=bijan #change bijan to your username
+GROUP_NAME=uucp #change to lock if you are runnting arch
+chgrp $GROUP_NAME /var/lock/lockdev
 chmod g+w /var/lock/lockdev
-usermod -a -G uucp mostafa #change bijan to your username
+usermod -a -G $GROUP_NAME $USERNAME
+usermod -a -G users $USERNAME
 cp "ZPreparing/50-embedded_devices.rules" /etc/udev/rules.d/
 cp ZPreparing/gtkterm.desktop /usr/share/applications/
 chmod +x /usr/share/applications/gtkterm.desktop
