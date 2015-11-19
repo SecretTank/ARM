c1200d43b5b3c665e9f4bc8ca9c3d63a2dc543db
ahmadr5674 <ahmadroshanfar@yahoo.com>
2015-11-19 13:44:53 +0330

    Fix readme

--- ReadMe/preparing.sh
+++ ReadMe/preparing.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-USERNAME=bijan #change bijan to your username
+USERNAME=ahmad #change bijan to your username
 GROUP_NAME=uucp #change to lock if you are runnting arch
 chgrp $GROUP_NAME /var/lock/lockdev
 chmod g+w /var/lock/lockdev
-<<<<<<< HEAD
 usermod -a -G $GROUP_NAME $USERNAME
 usermod -a -G users $USERNAME
-=======
-usermod -a -G uucp esi #change bijan to your username
->>>>>>> d3ce5cb851ae57c009feb6c140eb89471307b1ff
 cp "ZPreparing/50-embedded_devices.rules" /etc/udev/rules.d/
 cp ZPreparing/gtkterm.desktop /usr/share/applications/
 chmod +x /usr/share/applications/gtkterm.desktop
-echo "Please reboot to changes take effect"
 #add Qt color scheme
-cp 'ZPreparing/Cobalt.xml' '~/.config/QtProject/qtcreator/styles'
-chmod 777 '~/.config/QtProject/qtcreator/styles/Cobalt.xml'
+mkdir ~/.config/QtProject/qtcreator/styles
+cp 'ZPreparing/Cobalt.xml' ~/.config/QtProject/qtcreator/styles
+chmod 777 ~/.config/QtProject/qtcreator/styles/Cobalt.xml
+echo "Please reboot to changes take effect"
