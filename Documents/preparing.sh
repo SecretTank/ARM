86408b48375f69d70d4a8e334d8038b257da6bd4
bijanbina <bijanbina@gmail.com>
2016-09-14 20:20:17 +0430

    simpilify preparing process

--- Documents/preparing.sh
+++ Documents/preparing.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-USERNAME=ahmad #change bijan to your username
-GROUP_NAME=uucp #change to lock if you are runnting arch
+USERNAME=`whoami` #change bijan to your username
+
+# for newer systems answer yes
+# if you have any error about lock user group change all "lock" to uucp
+echo -n "Are you using arch linux [y/n]: "
+read answer
+if [ $answer == 'y' ]; then
+	GROUP_NAME=lock 
+else
+	GROUP_NAME=uucp
+fi
+
 chgrp $GROUP_NAME /var/lock/lockdev
 chmod g+w /var/lock/lockdev
 usermod -a -G $GROUP_NAME $USERNAME
 usermod -a -G users $USERNAME
-cp "ZPreparing/50-embedded_devices.rules" /etc/udev/rules.d/
-cp ZPreparing/gtkterm.desktop /usr/share/applications/
+cp "Resources/50-embedded_devices.rules" /etc/udev/rules.d/
+cp Resources/gtkterm.desktop /usr/share/applications/
 chmod +x /usr/share/applications/gtkterm.desktop
 #add Qt color scheme
 mkdir ~/.config/QtProject/qtcreator/styles
-cp 'ZPreparing/Cobalt.xml' ~/.config/QtProject/qtcreator/styles
+cp 'Resources/Cobalt.xml' ~/.config/QtProject/qtcreator/styles
 chmod 777 ~/.config/QtProject/qtcreator/styles/Cobalt.xml
 echo "Please reboot to changes take effect"
--- README.md
+++ README.md
@@ -4,10 +4,8 @@ ARM Example Projects in Linux
 ## How To Start
 1. Download CMSIS Library (named STM32CubeF4 for stm32 MCU families) from [ST website](http://www.st.com/web/en/catalog/tools/PF259242)
 2. To install CMSIS follow Install CMSIS Section in the page.
-3. open `Documents/preparing.sh` file with your favorite editor (like gedit).
-4. change "bijan" username to your Linux username
-5. run `Documents/preparing.sh` from your terminal with root access.
-6. Enjoy
+3. run `Documents/preparing.sh` from your terminal with root access.
+4. Enjoy
 
 ## Install CMSIS
 1. Create Directory `/usr/include/CMSIS` and `/usr/include/CMSIS/Device`
@@ -19,4 +17,3 @@ ARM Example Projects in Linux
 sudo cp "/home/bijan/Project/ARM/STM32CubeF4/Drivers/CMSIS/Include/*" /usr/include/CMSIS 
 ```
 
-- if you have any error about lock user group change all "lock" to uucp
