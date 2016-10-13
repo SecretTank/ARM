327d6f4a94d68bdbb7d96ed02e57ad447e996e5d
bijanbina <bijanbina@gmail.com>
2016-10-13 08:22:58 +0330

    Add accelerometer project

---
+++ 15_Accelerometer/SPI.h
@@ -0,0 +1,21 @@
+/* Functions to initialize, send, receive over USART
+
+   initUSART requires BAUD to be defined in order to calculate
+     the bit-rate multiplier.
+ */
+
+#ifndef _SPI_STM32_H_                   /* if not defined in Makefile... */
+#define _SPI_STM32_H_
+
+#include "stm32f4xx.h"
+#include "stm32f4_define.h"
+
+#define SPI_CHECK_ENABLED_RESP(SPIx)   if (!((SPIx)->CR1 & SPI_CR1_SPE)) {return 0;}
+#define SPI_IS_BUSY(SPIx) (((SPIx)->SR & (SPI_SR_TXE | SPI_SR_RXNE)) == 0 || ((SPIx)->SR & SPI_SR_BSY))
+#define SPI_WAIT(SPIx)            while (SPI_IS_BUSY(SPIx))
+
+uint8_t spi_transfer(uint8_t data);
+
+void spi_init(bool soft_cs);
+#endif
+
---
+++ 15_Accelerometer/stm32f4_define.h
@@ -0,0 +1,13 @@
+#ifndef _STM32_F$_DEFINE_H_
+#define _STM32_F$_DEFINE_H_
+
+#define 	_BV(bit)   (1 << (bit))
+#define 	bit_is_set(sfr, bit)   (sfr & bit)
+#define 	bit_is_clear(sfr, bit)   (!(sfr & bit))
+#define 	loop_until_bit_is_set(sfr, bit)   do { } while (bit_is_clear(sfr, bit))
+#define 	loop_until_bit_is_clear(sfr, bit)   do { } while (bit_is_set(sfr, bit))
+#define		TRUE	1 
+#define		FALSE	0 
+typedef		uint8_t bool;
+
+#endif
